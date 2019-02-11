d48e1be8df932e0a8fa4736a3e1e500ee1298914
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-02-11 14:18:02 +0000

    Regenerate dist and update package.json

--- dist/foxstorm-cli.js
+++ dist/foxstorm-cli.js
@@ -2,13 +2,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Yargs = require("yargs");
-const new_1 = require("./foxstorm/new");
-const serve_1 = require("./foxstorm/serve");
-const generate_1 = require("./foxstorm/generate");
+const new_1 = require("./foxstorm-cli/new");
+const serve_1 = require("./foxstorm-cli/serve");
+const generate_1 = require("./foxstorm-cli/generate");
 Yargs.demandCommand(1) // tslint:disable-line
     .strict()
     .command(new_1.newApp)
     .command(generate_1.generate)
     .command(serve_1.serve)
     .argv;
-//# sourceMappingURL=foxstorm.js.map
+//# sourceMappingURL=foxstorm-cli.js.map
